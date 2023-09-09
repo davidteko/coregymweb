cabc2c2d349b26ba48fc36cbac56220009ac3674
davidteko <144511832+davidteko@users.noreply.github.com>
2023-09-09 07:24:26 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function(manual){
+
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    contents.forEach((content) => {
+        content.classList.remove("active");
+    });
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
+}
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    });
+});
+
+let indexx=0;
+function next(){
+    slides[indexx].classList.remove('active');
+    indexx = (indexx +1) % slides.length;
+    slides[indexx].classList.add('active');
+}
+setInterval(next, 8000);
+
